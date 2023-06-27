331b60fe49594a3dd514aef445bc6423e5c5be9a
Daniil Deshkovich <daniil.deshkovich@gmail.com>
2023-06-27 12:46:38 +0300

    Add regions

--- Assets/NotMagicNumbers.cs
+++ Assets/NotMagicNumbers.cs
@@ -1,71 +1,79 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NotMagicNumbers : MonoBehaviour
 {
-    private int _sum;
-    private int _iterations;
+    #region Variables
 
-    public int range=50;
+    public TextMeshProUGUI endGame;
+    public TextMeshProUGUI message;
+
+    public int range = 50;
 
     public TextMeshProUGUI startMessage;
-    public TextMeshProUGUI message;
-    public TextMeshProUGUI endGame;
-    
-    void Start()
+    private int _iterations;
+    private int _sum;
+
+    #endregion
+
+    #region Unity lifecycle
+
+    private void Start()
     {
         startMessage.text = "Введите число от 1 до 9";
         message.text = SendMessage();
     }
 
-
-    void Update()
+    private void Update()
     {
         if (Input.anyKeyDown)
         {
             message.text = SendMessage();
         }
+
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
             _sum++;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad2))
         {
-            _sum+=2;
+            _sum += 2;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad3))
         {
-            _sum+=3;
+            _sum += 3;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad4))
         {
-            _sum+=4;
+            _sum += 4;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad5))
         {
-            _sum+=5;
+            _sum += 5;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad6))
         {
-            _sum+=6;
+            _sum += 6;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad7))
         {
-            _sum+=7;
+            _sum += 7;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad8))
         {
-            _sum+=8;
+            _sum += 8;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad9))
         {
-            _sum+=9;
+            _sum += 9;
         }
+
         if (_sum >= range)
         {
             endGame.text = $"Конец игры, затарчено попыток: {_iterations}";
         }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _iterations = 0;
@@ -75,10 +83,15 @@ public class NotMagicNumbers : MonoBehaviour
         }
     }
 
+    #endregion
+
+    #region Public methods
+
     public string SendMessage()
     {
         _iterations++;
         return $"Значене суммы {_sum}";
     }
-    
+
+    #endregion
 }
